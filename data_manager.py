26e420ae0ec0b3c04885cabf56103b2a6ee091eb
Ritche Reluao <34097542+ritchereluao@users.noreply.github.com>
2021-09-02 16:27:00 +0800

    Update data_manager.py

--- data_manager.py
+++ data_manager.py
@@ -1,16 +1,16 @@
-import requests
 from pprint import pprint
+import requests
 
-SHEETY_ENDPOINT = ""
-
+SHEETY_PRICES_ENDPOINT = YOUR SHEETY PRICES ENDPOINT
+SHEETY_USERS_ENDPOINT = YOUR SHEETY USERS ENDPOINT
 
 class DataManager:
-    # This class is responsible for talking to the Google Sheet.
+
     def __init__(self):
         self.destination_data = {}
 
     def get_destination_data(self):
-        response = requests.get(url=SHEETY_ENDPOINT)
+        response = requests.get(url=SHEETY_PRICES_ENDPOINT)
         data = response.json()
         self.destination_data = data["prices"]
         return self.destination_data
@@ -19,8 +19,18 @@ class DataManager:
         for city in self.destination_data:
             new_data = {
                 "price": {
-                    "iataCode": city["iataCode"],
+                    "iataCode": city["iataCode"]
                 }
             }
-            response = requests.put(url=f"{SHEETY_ENDPOINT}/{city['id']}", json=new_data)
-            pprint(response.text)
+            response = requests.put(
+                url=f"{SHEETY_PRICES_ENDPOINT}/{city['id']}",
+                json=new_data
+            )
+            print(response.text)
+
+    def get_customer_emails(self):
+        customers_endpoint = SHEETY_USERS_ENDPOINT
+        response = requests.get(url=customers_endpoint)
+        data = response.json()
+        self.customer_data = data["users"]
+        return self.customer_data
